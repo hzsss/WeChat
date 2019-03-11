401f61b063beb09b10e155a6fa30a17a1499d798
huangzishan <huangzishan123@gmail.com>
2019-03-11 09:28:49 +0800

    创建 wcdb 数据库

--- WeChat/Model/UserMessage.swift
+++ WeChat/Model/UserMessage.swift
@@ -10,6 +10,8 @@ import UIKit
 import WCDBSwift
 
 class UserMessage: TableCodable {
+    var database: Database = Database(withPath: "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!) + /userMessage.db")
+    
     var identifier: Int?
     var message: String?
     
@@ -21,6 +23,34 @@ class UserMessage: TableCodable {
     }
     
     init() {
+        do {
+            try database.create(table: "userMessageTable", of: UserMessage.self)
+        } catch  {
+            print("创建失败")
+        }
+    }
+    
+    func insertMessage(_ userMessage: UserMessage) {
+        do {
+            try database.insert(objects: userMessage, intoTable: "userMessageTable")
+        } catch {
+            print("插入失败")
+        }
+    }
+    
+    func deleteMessage(_ userMessage: UserMessage) {
+        do {
+            try database.delete(fromTable: "userMessageTable", where: UserMessage.Properties.identifier == userMessage.identifier!)
+        } catch {
+            print("删除失败")
+        }
+    }
+    
+    func updateMessage(_ userMessage: UserMessage) {
         
+    }
+    
+    func findMessage(_ userMessage: UserMessage) {
+
     }
 }
